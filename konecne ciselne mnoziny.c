5d0d127c6913f708f91afef7645e7fd6c481fc49
Mikhail Kharlamau <mikhail.kharlamau@gmail.com>
2024-04-02 16:45:35 +0200

    To co mame na konci cvicenia (nefunguje)

--- konecne ciselne mnoziny.c
+++ konecne ciselne mnoziny.c
@@ -15,19 +15,32 @@ void def_a_vypis_mnozinu(MNOZINA *a){
 	for (int i=0; i<a->l; i++){
 		 printf("%d ",a->a[i]);
 		 }
+	printf("\n");	 
 	}
 
 void vypis_mnoziny(MNOZINA *a){
 	
 	for (int i=0; i<a->l; i++){
 		 printf("%d ",a->a[i]);
-		 }	
+		 }
+	printf("\n");	 	
 	}
 
+MNOZINA zjednotenie_mnozin(MNOZINA *a,MNOZINA *b){
+	MNOZINA z;
 	
+	z.l=a->l+b->l;
+	for(int i=0;i<a->l;i++)
+		{z.a[i]=a->a[i];
+		}
+	return(z);	
+	}
+		
 main(){
 	MNOZINA a,b,c;
 	
 	def_a_vypis_mnozinu(&a);
 	def_a_vypis_mnozinu(&b);
+	c=zjednotenie_mnozin(&a,&b);
+	vypis_mnoziny(&c);
 }
