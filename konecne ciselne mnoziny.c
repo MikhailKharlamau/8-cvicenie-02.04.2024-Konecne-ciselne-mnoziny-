cb8eb96b722438b15cbb87d50bb205389662125a
Mikhail Kharlamau <mikhail.kharlamau@gmail.com>
2024-04-02 16:07:20 +0200

    proc def_a_vyp mnoziny

---
+++ konecne ciselne mnoziny.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct{
+	int *a;
+	int l;
+} MNOZINA;
+
+void def_a_vypis_mnozinu(MNOZINA *a){
+	printf("Zadaj pocet prvkov mnoziny:");
+	scanf("%d",&a->l);
+	for (int i=0; i<a->l; i++)
+		{printf("Zadaj %d-ty prvkok mnoziny:",i+1);
+		 scanf("%d",&a->a[i]);
+		}
+	for (int i=0; i<a->l; i++){
+		 printf("%d ",a->a[i]);
+		 }
+	}
+
+main(){
+	MNOZINA a;
+	
+	def_a_vypis_mnozinu(&a);
+}
